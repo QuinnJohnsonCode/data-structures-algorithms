728132f9a8dcd50d01d84998dd4890d094aa3683
Quinn Johnson <63220163+QuinnJohnsonCode@users.noreply.github.com>
2023-07-10 14:58:39 -0400

    Add in-order, post-order, pre-order traversals

---
+++ programs/algorithms/structures/tree/bt-postorder.ts
@@ -0,0 +1,20 @@
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
+
+    // recurse
+    walk(curr.left, path);
+    
+    // recurse
+    walk(curr.right, path);
+    
+    path.push(curr.value);
+
+    // post
+    return path;
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    return walk(head, []);
+}
